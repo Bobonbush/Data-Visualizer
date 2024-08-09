c43e76a4b19f3ea1b528a8ce93a2778956d3356e
Bobonbush <tdadung23@apcs.fitus.edu.vn>
2024-08-09 15:11:38 +0700

    exchange between computer nothing big

--- Code/Algorithms/graph.cpp
+++ Code/Algorithms/graph.cpp
@@ -1,1 +1,88 @@
 #include "graph.h"
+
+Edge::Edge(Node * _start, Node * _end, float _weight) {
+    start = _start;
+    end = _end;
+    weight = _weight;
+}
+
+Edge::~Edge() {
+    start = nullptr;
+    end = nullptr;
+}
+
+void Edge::Draw() {
+    start -> Draw();
+    end -> Draw();
+}
+
+void Edge::Update(float deltaTime, float MouseX , float MouseY) {
+    start -> Update(deltaTime, MouseX, MouseY);
+    end -> Update(deltaTime, MouseX, MouseY);
+}
+
+GRAPH::GRAPH(Camera* _camera, bool _Directed) {
+    camera = _camera;
+    Directed = _Directed;
+
+}
+
+GRAPH::~GRAPH() {
+    for (int i = 0; i < edges.size(); i++) {
+        delete edges[i];
+    }
+    for (int i = 0; i < nodes.size(); i++) {
+        delete nodes[i];
+    }
+}
+
+void GRAPH::Draw() {
+    for(Edge * edge : edges) {
+        edge -> Draw();
+    }
+
+}
+
+void GRAPH::Update(float deltaTime, float MouseX , float MouseY) {
+    for(Node * node : nodes) {
+        node -> Update(deltaTime, MouseX, MouseY);
+    }
+}
+
+void GRAPH::AddNode(Node * node) {
+    nodes.push_back(node);
+}
+
+void GRAPH::AddEdge(Edge * edge) {
+    edges.push_back(edge);
+}
+
+void GRAPH::RemoveNode(Node * node) {
+    for (int i = 0; i < nodes.size(); i++) {
+        if (nodes[i] == node) {
+            nodes.erase(nodes.begin() + i);
+            delete node;
+            return;
+        }
+    }
+}
+
+void GRAPH::RemoveEdge(Edge * edge) {
+    for (int i = 0; i < edges.size(); i++) {
+        if (edges[i] == edge) {
+            edges.erase(edges.begin() + i);
+            delete edge;
+            return;
+        }
+    }
+}
+
+void  GRAPH::RemoveEdge(Node * start, Node * end) {
+    for (int i = 0; i < edges.size(); i++) {
+        if (edges[i]->start == start && edges[i]->end == end) {
+            delete edges[i];
+            edges.erase(edges.begin() + i);
+            return;
+        }
+    }
+}
--- Code/Algorithms/graph.h
+++ Code/Algorithms/graph.h
@@ -5,27 +5,28 @@ class Edge {
         Node * start;
         Node * end;
         float weight;
-        bool Directed;
+        bool Directed = false;
         Edge(Node * _start, Node * _end, float _weight);
         ~Edge();
         void Draw();
-        void Update();
+        void Update(float deltaTime, float MouseX , float MouseY);
 };
 
 
 class GRAPH {
     std::vector<Edge*> edges;
     std::vector<Node*> nodes;
+    bool Directed = false;
+    Camera * camera;
     public:
-    GRAPH();
+    GRAPH(Camera* _camera, bool _Directed);
     ~GRAPH();
-    /*
+    
     void Draw();
-    void Update();
+    void Update(float deltaTime, float MouseX , float MouseY);
     void AddNode(Node * node);
     void AddEdge(Edge * edge);
     void RemoveNode(Node * node);
     void RemoveEdge(Edge * edge);
     void RemoveEdge(Node * start, Node * end);
-    */
 };
